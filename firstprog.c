bbf0f5a90cb3d2e5c7179f5ee8bd3745ef1fc8c2
Kathryn Woolard <shuricyber@gmail.com>
2020-06-21 10:49:57 -0400

    First program in Hacking:Art of Exp.

---
+++ firstprog.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	int i; 
+	for (i=0; i<10; i++0{ //loops for 10 times 
+	   printf("Hello, world!\n");//put string to the output
+	}
+	return 0;//OS has exited without errors 
+}
